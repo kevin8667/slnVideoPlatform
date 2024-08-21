100a52b22d5d53c0e054822079ba1bb67b8117c5
huangstar1110@gmail.com <huangstar1110@gmail.com>
2024-08-21 11:05:15 +0800

    Ticket_Update

---
+++ Backstage/Controllers/HallsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Backstage.Models;
+
+namespace Backstage.Controllers
+{
+    public class HallsController : Controller
+    {
+        private readonly VideoDBContext _context;
+
+        public HallsController(VideoDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Halls
+        public async Task<IActionResult> Index()
+        {
+            var videoDBContext = _context.Halls.Include(h => h.Cinema);
+            return View(await videoDBContext.ToListAsync());
+        }
+
+        // GET: Halls/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hall = await _context.Halls
+                .Include(h => h.Cinema)
+                .FirstOrDefaultAsync(m => m.HallsId == id);
+            if (hall == null)
+            {
+                return NotFound();
+            }
+
+            return View(hall);
+        }
+
+        // GET: Halls/Create
+        public IActionResult Create()
+        {
+            ViewData["CinemaId"] = new SelectList(_context.Cinemas, "CinemaId", "CinemaId");
+            return View();
+        }
+
+        // POST: Halls/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("HallsId,CinemaId,HallsName,SeatCapacity")] Hall hall)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(hall);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CinemaId"] = new SelectList(_context.Cinemas, "CinemaId", "CinemaId", hall.CinemaId);
+            return View(hall);
+        }
+
+        // GET: Halls/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hall = await _context.Halls.FindAsync(id);
+            if (hall == null)
+            {
+                return NotFound();
+            }
+            ViewData["CinemaId"] = new SelectList(_context.Cinemas, "CinemaId", "CinemaId", hall.CinemaId);
+            return View(hall);
+        }
+
+        // POST: Halls/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("HallsId,CinemaId,HallsName,SeatCapacity")] Hall hall)
+        {
+            if (id != hall.HallsId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(hall);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!HallExists(hall.HallsId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CinemaId"] = new SelectList(_context.Cinemas, "CinemaId", "CinemaId", hall.CinemaId);
+            return View(hall);
+        }
+
+        // GET: Halls/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hall = await _context.Halls
+                .Include(h => h.Cinema)
+                .FirstOrDefaultAsync(m => m.HallsId == id);
+            if (hall == null)
+            {
+                return NotFound();
+            }
+
+            return View(hall);
+        }
+
+        // POST: Halls/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var hall = await _context.Halls.FindAsync(id);
+            if (hall != null)
+            {
+                _context.Halls.Remove(hall);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool HallExists(int id)
+        {
+            return _context.Halls.Any(e => e.HallsId == id);
+        }
+    }
+}
