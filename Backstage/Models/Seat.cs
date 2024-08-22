6f21d314a95f158f3d015e1142f0ce803e5f738b
huangstar1110@gmail.com <huangstar1110@gmail.com>
2024-08-22 12:25:37 +0800

    UPDATE Cinema,Hall,Seat,Ticket

--- Backstage/Models/Seat.cs
+++ Backstage/Models/Seat.cs
@@ -2,6 +2,7 @@
 #nullable disable
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace Backstage.Models;
 
@@ -11,12 +12,16 @@ public partial class Seat
 
     public int? HallsId { get; set; }
 
+    [DisplayName("座位排數")]
     public string RowNumber { get; set; }
 
+    [DisplayName("座位號碼")]
     public int? SeatNumber { get; set; }
 
+    [DisplayName("座位狀態")]
     public string SeatStatus { get; set; }
 
+    [DisplayName("影院名稱")]
     public virtual Hall Halls { get; set; }
 
     public virtual ICollection<Ticket> Tickets { get; set; } = new List<Ticket>();
