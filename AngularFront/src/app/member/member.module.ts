926174e2b98c569588c93b940b223a75a70f7a81
Jarry6304 <jarry6304@gmail.com>
2024-09-17 16:43:49 +0800

    小東西

--- AngularFront/src/app/member/member.module.ts
+++ AngularFront/src/app/member/member.module.ts
@@ -37,7 +37,7 @@ const routes: Routes = [
   { path: 'mmain', component: MmainComponent },
   { path: 'message', component: MessageComponent },
   { path: 'history', component: HistoryComponent },
-  { path: 'Roulette', component: RouletteComponent },
+  { path: 'roulette', component: RouletteComponent },
   { path: '**', component: MmainComponent },
 ];
 
