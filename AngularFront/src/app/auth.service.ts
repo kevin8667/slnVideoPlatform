92c9240e01c6d2e368b0d32dc2de90b5266e52e3
Jarry6304 <jarry6304@gmail.com>
2024-09-19 12:48:46 +0800

    auth.service

--- AngularFront/src/app/auth.service.ts
+++ AngularFront/src/app/auth.service.ts
@@ -2,22 +2,51 @@ import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of, BehaviorSubject } from 'rxjs';
-import { catchError, tap, shareReplay } from 'rxjs/operators';
+import { state } from '@angular/animations';
 
 // 定義接口來處理返回的結果和錯誤狀態
-
+export interface MemberIdResponse {
+  address: string;
+  banned: null;
+  bindingLine: string;
+  birth: Date;
+  email: string;
+  fidoCredentialID: null;
+  fidoEnabled: null;
+  gender: string;
+  grade: string;
+  jwtToken: string;
+  lastLoginDate: Date;
+  lineUserId: string;
+  memberID: 3;
+  memberIdentity: null;
+  memberName: string;
+  nickName: string;
+  password: string;
+  phone: string;
+  photoPath: string;
+  point: number;
+  process: 0;
+  registerDate: Date;
+  status: string;
+  updateTime: Date;
+  updateUser: null;
+  error: boolean;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   private apiUrl = 'https://localhost:7193/api/Member/GetMemberId'; // 替換為您的實際 API URL
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router) {}
 
   private isLogin = new BehaviorSubject<boolean>(this.hasToken());
 
-  private memberBehaviorSubject = new BehaviorSubject<any | null>(this.getMemberData());
+  private memberBehaviorSubject = new BehaviorSubject<MemberIdResponse | null>(
+    this.getMemberData()
+  );
 
   removeCookie(name: string): void {
     // 设置一个过期时间为过去的时间
@@ -30,12 +59,13 @@ export class AuthService {
     return this.memberBehaviorSubject.asObservable();
   }
 
-  getMemberData(): any | null {
-    console.log("getMemberData by cookie");
+  getMemberData(): MemberIdResponse | null {
+    console.log('getMemberData by cookie');
     const cookieValue = this.getCookie('MemberData');
     if (cookieValue) {
       try {
-        return JSON.parse(cookieValue);
+        const memberValue = JSON.parse(cookieValue);
+        return memberValue;
       } catch (error) {
         console.error('解析 MemberData Cookie 失敗:', error);
         return null;
@@ -44,11 +74,9 @@ export class AuthService {
     return null;
   }
 
-  
-  SetMemberData(data:any): void {
+  SetMemberData(data: MemberIdResponse): void {
     this.memberBehaviorSubject.next(data);
     this.setCookie('MemberData', JSON.stringify(data), 1);
-
   }
 
   hasToken(): boolean {
@@ -60,9 +88,7 @@ export class AuthService {
     this.removeCookie('JwtToken');
     this.removeCookie('MemberData');
     this.memberBehaviorSubject.next(null);
-
-    this.router.navigateByUrl('login');
-
+    location.reload();
   }
   SetLoginValue(): void {
     this.isLogin.next(true);
@@ -83,18 +109,18 @@ export class AuthService {
     return null; // 返回 null 如果没有找到该 cookie
   }
 
-
-
   loginWithLine(binding: boolean) {
     debugger;
     const lineLoginUrl = 'https://access.line.me/oauth2/v2.1/authorize';
     const clientId = '2006329488';
-    const redirectUri = encodeURIComponent('http://localhost:4200/#/auth/callback');
+    const redirectUri = encodeURIComponent(
+      'http://localhost:4200/#/auth/callback'
+    );
     const state = '3'; // 生成一個隨機的 state 參數
     const scope = 'openid profile';
 
     const authUrl = `${lineLoginUrl}?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&state=${state}&scope=${scope}`;
-    this.setCookie("Binding", binding ? "Y" : "N", 1);
+    this.setCookie('Binding', binding ? 'Y' : 'N', 1);
     window.location.href = authUrl;
   }
 
@@ -108,5 +134,4 @@ export class AuthService {
     const expiresString = 'expires=' + expires.toUTCString();
     document.cookie = `${name}=${value}; ${expiresString}; path=/`;
   }
-
 }
